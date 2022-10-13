08620fa1bbf80193792b3c8c178e6466e6f39af1
Matthew <mtgreff5@gmail.com>
2022-10-13 17:17:09 -0400

    commit tree a4f5b4671c26b34678432472e2b338a2c603ba51

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feetTraveled){
+  if (feetTraveled <= 400){
+    return "This one is on me!"
+  } else if (feetTraveled > 400 && feetTraveled <= 2000){
+    return "That will be twenty bucks."
+  } else if (feetTraveled > 2000 && feetTraveled <= 2500){
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(whereTo){
+  return whereTo === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(howMuch){
+  switch (howMuch) {
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+      break;
+  }
+}  
